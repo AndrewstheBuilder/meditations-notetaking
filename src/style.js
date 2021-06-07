9407526fef6eb73259a8a63553444d7210cec4ac
Andrews Peter <51387040+AndrewstheBuilder@users.noreply.github.com>
2021-06-07 11:54:28 -0400

    Initial commit

---
+++ src/style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import React from 'react';
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
+
+export const StyledInput = styled.textarea`
+    color:purple;
+`
+export const StyledInputDiv = styled(
+    ({...rest}) => (
+        <div contentEditable='true' {...rest}>
+
+        </div>
+    )
+)`
+ height: '50%';
+`
+
+export const StyledPlaceHolder = styled(
+    ({...rest}) => (
+        <div contentEditable='true' classes={{root:'root'}} {...rest}>
+
+        </div>
+    )
+)`
+    color:'#fff';
+`;
+
+export const MyEditor = () => {
+    const [editorState, setEditorState] = React.useState(() =>
+      EditorState.createEmpty()
+    );
+
+    const editor = React.useRef(null);
+    function focusEditor() {
+      editor.current.focus();
+    }
+
+    return (
+      <div
+        style={{margin: '5px', padding: '10px', minHeight: "90vh", cursor: "text" }}
+        onClick={focusEditor}
+      >
+        <Editor
+          ref={editor}
+          editorState={editorState}
+          onChange={setEditorState}
+          placeholder="Write something!"
+        />
+      </div>
+    );
+  }
+
+  
